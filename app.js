b033cb8a91838eedc90b005e397836a22cb70361
Nikolaos Tsoukas <nikolaostsoukas@Nikolaoss-iMac.local>
2018-03-18 23:55:33 -0600

    Adding user authentication - installing packages

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ var express    = require('express'),
     app        = express(),
     bodyParser = require('body-parser'),
     mongoose   = require("mongoose"),
+    passport   = require("passport"),
+    LocalStrategy = require("passport-local"),
     Campground = require("./models/campground"),
     Comment    = require("./models/comment"),
+    User       = require("./models/user"),
     seedDB     = require("./seeds");
 
 //APP CONFIG
